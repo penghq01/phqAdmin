cbc7777d84557f3fb7494afaf22aa94a78ab5c99
penghq <2443445161@qq.com>
2020-10-26 17:48:46 +0800

    2020-10-16-1748

--- admin-html/src/lib/exportExcel.js
+++ admin-html/src/lib/exportExcel.js
@@ -4,12 +4,13 @@ import {MessageBox} from 'element-ui';
 
 function ExportExcel(tHeader = [], filterVal = [], excelData = [], fileName) {
     import('@/vendor/Export2Excel').then(excel => {
-        const data = FormatJson(filterVal, excelData);
+        const datas = FormatJson(filterVal, excelData);
         excel.export_json_to_excel({
             header: tHeader,
-            data,
+            data:datas,
             filename: fileName,
         });
+        message.loading.hide();
     });
 }
 
@@ -22,18 +23,25 @@ function FormatJson(filterVal, jsonData) {
 }
 //formatArr[{fieldName：字段名，name：字段的中文名，handle：处理函数 }]
 export default {
-    //fieldName：字段名，name：字段的中文名，handle：处理函数
-    field(fieldName, name, handle = val => {
+    /*
+    * fieldName：字段名
+    * name：字段的中文名
+    * handle：处理函数
+    **/
+    field(nickName,fieldName, handle = val => {
         return val
     }) {
         return {
             "field_name": fieldName,
-            "name": name,
+            "name": nickName,
             "handle": handle
         };
     },
-    excelHandle(url, formatArr = [], {params = {}, fileName = "WEB导出Excel数据", showIndex = false, indexName = "序号",pageSize=10}={}) {
-        MessageBox.prompt(`您要导出几页数据（每页${pageSize}条）？请输入页数`, '提示', {
+    /*
+    *formatArr：field数组
+    **/
+    excelHandle(url, formatArr = [], {params = {}, fileName = "WEB导出Excel数据", showIndex = false, indexName = "序号",pageSize=10,page=1}={}) {
+        MessageBox.prompt(`您要导出几页数据（每页${pageSize}条，从当前页开始导出），？请输入页数`, '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
         }).then(({value}) => {
@@ -41,10 +49,10 @@ export default {
             if (size > 0) {
                 message.loading.show("数据加载中……，请耐心等待");
                 let page_size = size * pageSize;
-                http.post(`${url}/${page_size}/1`,params).then(data => {
+                http.post(`${url}/${page_size}/${page}`,params).then(data => {
                     message.loading.hide();
                     let excelData = [];
-                    if (data.data.length > 0) {
+                    if (data.data instanceof Array && data.data.length > 0) {
                         message.loading.show("数据处理中……，请耐心等待");
                         let tHeader = []
                         let filterVal = []
@@ -68,17 +76,43 @@ export default {
                                 if (nameArr.length > 1) {
                                     let handleParams = []
                                     nameArr.forEach(paramsField => {
-                                        handleParams.push(row[paramsField])
+                                        let objArr =  paramsField.split(".");
+                                        if(objArr.length>1){
+                                            let paramsObjData=null;
+                                            objArr.forEach(obj=>{
+                                                if(paramsObjData == null){
+                                                    paramsObjData=row[obj];
+                                                }else{
+                                                    paramsObjData = paramsObjData[obj];
+                                                }
+                                            });
+                                            handleParams.push(paramsObjData)
+                                        }else{
+                                            handleParams.push(row[paramsField])
+                                        }
+
                                     });
                                     fieldVal=field.handle(handleParams);
                                 } else {
-                                    fieldVal=field.handle(row[field_name]);
+                                    let objArr =  field_name.split(".");
+                                    if(objArr.length>1){
+                                        let paramsObjData=null;
+                                        objArr.forEach(obj=>{
+                                            if(paramsObjData == null){
+                                                paramsObjData=row[obj];
+                                            }else{
+                                                paramsObjData = paramsObjData[obj];
+                                            }
+                                        });
+                                        fieldVal=field.handle(paramsObjData);
+                                    }else{
+                                        fieldVal=field.handle(row[field_name]);
+                                    }
                                 }
                                 tem[`${field_name}_${index}`]=fieldVal;
                             })
                             excelData.push(tem);
                         });
-                        message.loading.hide();
                         ExportExcel(tHeader, filterVal, excelData, fileName);
                     } else {
                         message.msg.info("没有获取到数据");
@@ -86,7 +120,6 @@ export default {
                 }).catch(err => {
                     message.loading.hide();
                 });
-
             } else {
                 message.msg.error("最少导出一页数据");
             }
